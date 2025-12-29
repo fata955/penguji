7c1ce3fbe43e65f5b6cd043f225d0a8e12891bbd
fata955 <fatahillah955@gmail.com>
2025-12-29 20:51:28 +0800

    db

--- lib/dbh.inc.php
+++ lib/dbh.inc.php
@@ -4,7 +4,6 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $database = "spm";
-$charset = 'utf8mb4';
 // $servername = "localhost";
 // $username = "root";
 // $password = "nadirad3mi208";
